c313321e75101eda4110ea914cb07f10d9a05939
Alexander Matveev <39197341+amdev9@users.noreply.github.com>
2025-11-21 18:04:42 +0300

    add typescript definition for external request

--- src/workflows/externalApiRedisStep.ts
+++ src/workflows/externalApiRedisStep.ts
@@ -9,7 +9,21 @@ interface ExternalApiStepInput {
     to?: string;
 }
 
-function calculateTotalRes(amount: string, data: any): string {
+interface ExternalApiResponse {
+    result: string,
+    documentation: string,
+    terms_of_use: string,
+    time_last_update_unix: number,
+    time_last_update_utc: string,
+    time_next_update_unix: number,
+    time_next_update_utc: string,
+    base_code: string,
+    target_code: string,
+    conversion_rate: number,
+    conversion_result: number
+}
+
+function calculateTotalRes(amount: string, data: ExternalApiResponse): string {
     // console.log("data", data)
     const price = new Decimal(amount);
     const conversion_rate = new Decimal(data.conversion_rate);
